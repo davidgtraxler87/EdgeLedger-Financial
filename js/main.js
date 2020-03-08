a200ee33c4df14bcba0ce7a4c70d0877a4928862
David Traxler <david.g.traxler87@gmail.com>
2020-03-07 22:40:39 -0600

    Smooth Scrolling with JS

--- js/main.js
+++ js/main.js
@@ -11,5 +11,29 @@ function initMap() {
     const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
+//Stick Menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.85;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
 
+
+//Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if(this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
 
